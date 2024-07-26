95afe17f0baa90ae360a0bc3002ed42a5530b69b
Dwayne Paisley-Marshall <dwaynepaisley-marshall@dwaynes-mbp.kpn>
2024-07-26 21:55:23 +0200

    Re ordered files

--- api/index.js
+++ api/index.js
@@ -65,10 +65,7 @@ app.post('/api/chat', async (req, res) => {
   }
 });
 
-// The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'src', 'build', 'index.html'));
-});
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
